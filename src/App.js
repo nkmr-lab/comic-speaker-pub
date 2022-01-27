8f0363082fde0554b88eb2e22aacb682e7d0d4d9
nonaka <electro0701@gmail.com>
2022-01-27 16:18:17 +0900

    櫻井CC6の論文内容を反映

--- src/App.js
+++ src/App.js
@@ -11,44 +11,44 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="dark" expand="lg">
-       <Navbar.Brand style={{color: "#ffffff"}}>セリフ発話者データセット配布サイト</Navbar.Brand>
+        <Navbar.Brand style={{ color: "#ffffff" }}>セリフ発話者データセット配布サイト</Navbar.Brand>
       </Navbar>
       <Container>
-        <Row style={{padding: "2em 0rem 1em"}}>
+        <Row style={{ padding: "2em 0rem 1em" }}>
           <div>
-          <h1>概要</h1>
-          <p>
-            本データセットは<a href="http://www.manga109.org" target="_blank">Manga109</a>の漫画に登場するセリフに対して発話者であるキャラクタのアノテーション情報を収集したものです．
-            漫画のアノテーションを付与するための<a href="https://exam-speaker.nkmr.io/demo/" target="_blank">システム</a>により，
-            109冊の漫画に登場する147,918件のセリフに対して2名のデータ収集者が発話者となるキャラクタとの対応付を行なったものになります．
-            {/* <br/> */}
-            なお，実際の漫画画像およびその他のアノテーション情報についてはManga109のデータ配布サイトよりダウンロードして下さい．
-          </p>
+            <h1>概要</h1>
+            <p>
+              本データセットは<a href="http://www.manga109.org" target="_blank">Manga109</a>の漫画に登場するセリフに対して発話者であるキャラクタのアノテーション情報を収集したものです．
+              漫画のアノテーションを付与するための<a href="https://exam-speaker.nkmr.io/demo/" target="_blank">システム</a>により，
+              109冊の漫画に登場する147,918件のセリフに対して平均約5名のデータ収集者が発話者となるキャラクタとの対応付を行なったものになります．
+              {/* <br/> */}
+              なお，実際の漫画画像およびその他のアノテーション情報についてはManga109のデータ配布サイトよりダウンロードして下さい．
+            </p>
           </div>
         </Row>
-        <Row style={{padding: "1em 0em"}}>
+        <Row style={{ padding: "1em 0em" }}>
           <div>
-          <h1>ダウンロード</h1>
-          <p>
-            以下のリンクからダウンロードできます<br/>
-            <ul>
-              <li><a href="https://nkmr.io/comic/speaker-dataset/Comic-Speaker-Dataset.tar.gz">Comic-Speaker-Dataset.tar.gz</a></li>
-            </ul>
-          </p>
+            <h1>ダウンロード</h1>
+            <p>
+              以下のリンクからダウンロードできます<br />
+              <ul>
+                <li><a href="https://nkmr.io/comic/speaker-dataset/Comic-Speaker-Dataset.tar.gz">Comic-Speaker-Dataset.tar.gz</a></li>
+              </ul>
+            </p>
           </div>
         </Row>
-        <Row style={{padding: "1em 0em 0em"}}>
+        <Row style={{ padding: "1em 0em 0em" }}>
           <div>
-          <h1>データ内容</h1>
-          データセットには2種類のcsvファイルがあります．
+            <h1>データ内容</h1>
+            データセットには2種類のcsvファイルがあります．
           </div>
         </Row>
         <Row>
-          <ui><li style={{fontWeight: "bold", fontSize: "1.2em", padding: "0.5em 0em"}}>annotation.csv</li></ui>
+          <ui><li style={{ fontWeight: "bold", fontSize: "1.2em", padding: "0.5em 0em" }}>annotation.csv</li></ui>
         </Row>
         <Row>
-          
-          <Col lg={{span: 8, offset: 2}}>
+
+          <Col lg={{ span: 8, offset: 2 }}>
             <Table striped bordered hover responsive>
               <thead>
                 <tr>
@@ -97,8 +97,8 @@ function App() {
                 </tr>
               </tbody>
             </Table>
-            <div style={{textAlign: "center", fontWeight: "bold"}}>.<br/>.<br/>.</div>
-            <ui style={{listStyleType: "circle"}}>
+            <div style={{ textAlign: "center", fontWeight: "bold" }}>.<br />.<br />.</div>
+            <ui style={{ listStyleType: "circle" }}>
               <li>title: 漫画のタイトル</li>
               <li>index: セリフが登場するページ</li>
               <li>text_id: セリフのID</li>
@@ -107,15 +107,15 @@ function App() {
             </ui>
           </Col>
         </Row>
-        <Row style={{padding: "0.5em 0em"}}>
+        <Row style={{ padding: "0.5em 0em" }}>
           セリフと発話者であるキャラクタを対応付けたデータになります．"text_id"および"character_id"は，Manga109に含まれるアノテーションと対応しています．
-          1つのセリフには2名のアノテーション付与者がいます．
+          1つのセリフには平均約5名のアノテーション付与者がいます．
           また，1つのセリフに複数のキャラクタが対応付けられていることもあります．
-          <br/><span style={{color: "#ff0000"}}>※キャラクターのIDには　不明："99999999"，ナレーション："11111111"，その他："000other"　の3つのIDが追加されています．</span>
+          <br /><span style={{ color: "#ff0000" }}>※キャラクターのIDには　不明："99999999"，ナレーション："11111111"，その他："000other"　の3つのIDが追加されています．</span>
         </Row>
-        <Row style={{padding: "0em 0em 1em"}}>
-          
-        {/* <Col>
+        <Row style={{ padding: "0em 0em 1em" }}>
+
+          {/* <Col>
             <Table striped bordered hover responsive>
               <thead>
                 <tr>
@@ -144,21 +144,22 @@ function App() {
             </Table>
           </Col> */}
           <div>
-          <ui><li style={{fontWeight: "bold", fontSize: "1.2em", padding: "0.5em 0em"}}>list.csv</li></ui>
+            <ui><li style={{ fontWeight: "bold", fontSize: "1.2em", padding: "0.5em 0em" }}>list.csv</li></ui>
             アノテーション付与者が担当した作品のリストになります．
             3列目の"total_tasks"はその作品に対するアノテーション付与の合計回数になります．
           </div>
         </Row>
-        <Row style={{padding: "1em 0em"}}>
+        <Row style={{ padding: "1em 0em" }}>
           <div>
             <h1>文献</h1>
             <p>本データセットを使用した論文を執筆される際には，下記文献を引用ください．</p>
             <ul>
-            <li>阿部 和樹, 中村 聡史. コミックのセリフと発話者対応付けデータセットの構築とその困難性, 第3回 コミック工学研究会, pp.7-12, 2020.</li>
+              <li>阿部 和樹, 中村 聡史. コミックのセリフと発話者対応付けデータセットの構築とその困難性, 第3回 コミック工学研究会, pp.7-12, 2020.</li>
+              <li>櫻井 翼, 伊藤 理紗, 阿部 和樹, 中村 聡史. 漫画のセリフと発話者対応付けデータセットの構築とその分析, 第6回 コミック工学研究会, pp.11-17, 2021.</li>
             </ul>
           </div>
         </Row>
-        <Row style={{padding: "1em 0em"}}>
+        <Row style={{ padding: "1em 0em" }}>
           <div>
             <h1>リンク</h1>
             <ui>
@@ -168,8 +169,8 @@ function App() {
             </ui>
           </div>
         </Row>
-        <Row style={{padding: "1em 0em"}}>
-          <div style={{textAlign: "center"}}>
+        <Row style={{ padding: "1em 0em" }}>
+          <div style={{ textAlign: "center" }}>
             明治大学総合数理学部 <a href="nkmr-lab.org" target="_blank">中村研究室</a>
           </div>
         </Row>
