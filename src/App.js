9be766d6c09048afd3b7c06a91c9a87e00cc331f
abekzk <29366832+abekzk@users.noreply.github.com>
2023-06-03 15:55:59 +0900

    docs: リンクを各論文リポジトリのページに修正

--- src/App.js
+++ src/App.js
@@ -164,8 +164,8 @@ function App() {
             <h1>リンク</h1>
             <ui>
               <li><a href="http://www.manga109.org" target="_blank">Manga109 - Japanese Manga Dataset -</a></li>
-              <li><a href="https://dl.nkmr-lab.org/papers/208" target="_blank">漫画における台詞発話者の自動判定に向けた技術的困難性による整理とデータセット構築手法の検討 - 明治大学 中村聡史研究室 論文レポジトリ</a></li>
-              <li><a href="https://exam-speaker.nkmr.io/demo" target="_blank">発話者アノテーション付与・デモページ</a></li>
+              <li><a href="https://dl.nkmr-lab.org/papers/240" target="_blank">コミックのセリフと発話者対応付けデータセットの構築とその困難性 - 明治大学 中村聡史研究室 論文レポジトリ</a></li>
+              <li><a href="https://dl.nkmr-lab.org/papers/403" target="_blank">A Method to Annotate Who Speaks a Text Line in Manga and Speaker-Line Dataset for Manga109 - 明治大学 中村聡史研究室 論文レポジトリ</a></li>
             </ui>
           </div>
         </Row>
